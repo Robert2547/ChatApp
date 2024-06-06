f57249a3aefc3db523505a0d0c7cc2afb6ebbc52
Robert2547 <91036825+Robert2547@users.noreply.github.com>
2024-06-06 17:16:49 -0400

    Setting up router

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-const express = require("express")
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Middleware
+app.use("/api/auth", authRoutes);
+
+// Server
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT + "...");
+});
